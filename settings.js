7cfe2f3c8cfd7d339ba98438da163c9881b9976b
dasunnuwantha74-ux <dasunnuwantha74@gmail.com>
2026-01-14 19:28:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
+උconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "TAxmSBZJ#x_Cn1Z38nS17GfO6tB50lHx6qF7e9KtDJcMxATjIy6E", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94701698511",
+DEV: process.env.DEV || "94701698511",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
